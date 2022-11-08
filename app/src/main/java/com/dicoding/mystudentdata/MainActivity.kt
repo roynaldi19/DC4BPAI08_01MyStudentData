5ebc8f474aa7d888dbb2e2b5b47bb38d3fb9bfe8
roynaldi19 <roynaldi19@gmail.com>
2022-11-08 18:17:46 +0700

    many to many

--- app/src/main/java/com/dicoding/mystudentdata/MainActivity.kt
+++ app/src/main/java/com/dicoding/mystudentdata/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.mystudentdata.adapter.StudentAndUniversityAdapter
 import com.dicoding.mystudentdata.adapter.StudentListAdapter
+import com.dicoding.mystudentdata.adapter.StudentWithCourseAdapter
 import com.dicoding.mystudentdata.adapter.UniversityAndStudentAdapter
 import com.dicoding.mystudentdata.databinding.ActivityMainBinding
 
@@ -85,9 +86,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun getStudentWithCourse() {
-
+        val adapter = StudentWithCourseAdapter()
+        binding.rvStudent.adapter = adapter
+        mainViewModel.getAllStudentWithCourse().observe(this) {
+            Log.d(TAG, "getStudentWithCourse: $it")
+            adapter.submitList(it)
+        }
     }
 
     companion object {
